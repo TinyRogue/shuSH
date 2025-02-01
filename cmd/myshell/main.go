36a1760fd6b98696da190f6420c39ce42e4a598d
Oskar Mikus <oskarmikus@proton.me>
2025-02-01 01:34:23 +0100

    Add REPL template

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,11 +8,13 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
 	scanner := bufio.NewScanner(os.Stdin)
-	if ok := scanner.Scan(); !ok {
-		log.Fatalf("An error occurred: %v", scanner.Err())
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		if ok := scanner.Scan(); !ok {
+			log.Fatalf("An error occurred: %v", scanner.Err())
+		}
+		command := scanner.Text()
+		fmt.Printf("%s: command not found\n", command)
 	}
-	command := scanner.Text()
-	fmt.Printf("%s: command not found\n", command)
 }
