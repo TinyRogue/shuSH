ca5f538b8d2e3990be4d5a5874ec296680ca1c08
Oskar Mikus <oskarmikus@proton.me>
2025-02-03 01:10:05 +0100

    Add handling cd

---
+++ internal/commands/cd.go
@@ -0,0 +1,16 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+)
+
+func (command *Command) handleChangeDirectory() {
+	path := ""
+	if len(command.subcommands) > 0 {
+		path = command.subcommands[0]
+	}
+	if err := os.Chdir(path); err != nil {
+		fmt.Printf("cd: %s: No such file or director\n", path)
+	}
+}
